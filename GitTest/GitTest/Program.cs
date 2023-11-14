5bd2bac3a6095a28ec9ce7a2b970e0465d20dd43
Alexandr Gorelkin <aleksandr-gorelkin@yandex.ru>
2023-11-14 05:12:26 +0300

    addF

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -9,10 +9,19 @@ namespace GitTest
             string h = "Hello";
             string w = "World";
             char i = '!';
-
+            int a = 2;
+            int b = 4;
 
             Console.WriteLine($"{h} {w}{i}");
+
+            int c = a * b;
+
+            Console.Write($"{c}");
+            
             Console.ReadKey();
+
+            
+            P
         }
     }
 }
